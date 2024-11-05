27d793b5b2342650fc770992ea52cb85a996f1a2
Jethrolopwus <lopwusjethro92@gmail.com>
2024-11-05 12:27:25 +0100

    UPDATE:update alumni page and some fixes accross the page

--- blockfuse-website/src/components/NewsLetters.tsx
+++ blockfuse-website/src/components/NewsLetters.tsx
@@ -5,8 +5,8 @@ import React from 'react';
 const NewsLetters = () => {
   return (
     <div className='lg:max-w-[816px] pb-10 mt-32 w-full flex flex-col justify-center mx-auto gap-20 items-center px-4'>
-      <h2 className='text-4xl leading-tight font-semibold text-center'>Subscribe to Our Newsletter</h2>
-      <p className='text-xl leading-relaxed text-center'>
+      <h2 className='text-4xl leading-tight font-semibold text-center dark:text-white'>Subscribe to Our Newsletter</h2>
+      <p className='text-xl leading-relaxed text-center dark:text-white'>
         Stay ahead in the world of blockchain! Get the latest updates, insights, and exclusive resources from Blockfuse Labs delivered straight to your inbox. Don’t miss out—subscribe today!
       </p>
       <div className='flex flex-col gap-4 w-full max-w-[816px]'>
