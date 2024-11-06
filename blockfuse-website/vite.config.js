f567a998bbeff411ff05986e4ef41604a3608ac7
Emmanuel Doji <devopsnormad@gmail.com>
2024-11-06 15:16:16 +0100

    fixes

--- blockfuse-website/vite.config.js
+++ blockfuse-website/vite.config.js
@@ -4,15 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: [
-        'lucide-react', 
-        '@babel/generator', 
-        '@babel/template',
-        '@babel/traverse',
-        'prop-types'
-      ],
-    },
-  },
 })
