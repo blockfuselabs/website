ab484f040259050899f57bf8a5d2562f3f7871d3
Jethrolopwus <lopwusjethro92@gmail.com>
2024-11-06 11:29:34 +0100

    UDATE: fixes on alumni page

--- blockfuse-website/src/components/AluminiCohorts.tsx
+++ blockfuse-website/src/components/AluminiCohorts.tsx
@@ -20,25 +20,17 @@ const AluminiCohorts = () => {
         {/* Text Content */}
         <div className="relative text-center z-10">
           <header>
-            <h1 className="text-5xl md:text-6xl dark:text-white">
-              Unlock the Future with{" "}
-              <span className="text-purple-500 font-bold">Blockchain</span>
+            <h1 className="text-5xl  md:text-5xl dark:text-white">
+              Meet Our Alumni{" "}
+              <span className="text-purple-500 font-bold">At Blockfuselabs</span>
             </h1>
           </header>
           <div className="-space-y-1">
-            <p className="mt-4 text-lg md:text-xl dark:text-gray-300">
-              Transform your skills and career with comprehensive
-            </p>
-            <p className="mt-4 text-lg md:text-xl dark:text-gray-300">
-              training designed to lead the digital revolution
+            <p className="mt-4 text-lg md:text-xl w-full md:w-3/4  mx-auto dark:text-gray-300">
+            Discover the bright minds shaping the blockchain space! Our alumni are pioneers, creators, and leaders who drive innovation and inspire the next generation in the world of decentralized technology.Blockfuse Labs Alumni is where graduates connect, collaborate, and continue to drive the future of decentralized technology.
             </p>
           </div>
-          <div className="mt-8 flex justify-center">
-            <Button>
-              Explore our bootcamps
-              <MoveRight />
-            </Button>
-          </div>
+
         </div>
       </section>
     </div>
