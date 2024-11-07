b5773a6f866915d461aad422f8c58bbea8e34d55
mananuf <bmananaf@gmail.com>
2024-11-07 22:25:03 +0100

    applications: adds controller methods to get applications

--- backend/controllers/applicationController.js
+++ backend/controllers/applicationController.js
@@ -1,6 +1,127 @@
 const { Application } = require('../models/application');
+const { Op } = require('sequelize');
 
 class ApplicationController {
+
+    static async getAll(req, res) {
+        try {
+
+            const {
+            fullname,
+            email,
+            application_type,
+            country,
+            createdAt,
+            page = 1,
+            limit = 25,
+            sortBy = 'application_type',
+            sortOrder = 'DESC',
+            search
+            } = req.query;
+
+            const offset = (page - 1) * limit;
+
+            const whereClause = {};
+            
+            if (createdAt) {
+            whereClause.start_date = { [Op.gte]: createdAt };
+            }
+
+            if (search) {
+            whereClause[Op.or] = [
+                { fullname: { [Op.iLike]: `%${search}%` } },
+                { email: { [Op.iLike]: `%${search}%` } },
+                { application_type: { [Op.iLike]: `%${search}%` } },
+                { country: { [Op.iLike]: `%${search}%` } },
+            ];
+            }
+
+            const allowedSortFields = ['id', 'fullname', 'email', 'country', 'application_type', 'createdAt'];
+            const validSortBy = allowedSortFields.includes(sortBy) ? sortBy : 'application_type';
+
+            const validSortOrder = ['ASC', 'DESC'].includes(sortOrder.toUpperCase()) 
+            ? sortOrder.toUpperCase() 
+            : 'DESC';
+
+            const { count, rows: applications } = await Application.findAndCountAll({
+            where: whereClause,
+            order: [[validSortBy, validSortOrder]],
+            limit: parseInt(limit),
+            offset: parseInt(offset)
+            });
+
+            const totalPages = Math.ceil(count / limit);
+            const currentPage = parseInt(page);
+            const hasNextPage = currentPage < totalPages;
+            const hasPrevPage = currentPage > 1;
+
+            res.status(200).json({
+            message: 'applications retrieved successfully.',
+            data: {
+                applications,
+                pagination: {
+                total: count,
+                per_page: parseInt(limit),
+                current_page: currentPage,
+                total_pages: totalPages,
+                has_next_page: hasNextPage,
+                has_prev_page: hasPrevPage
+                },
+                filters: {
+                fullname,
+                country,
+                application_type,
+                search
+                },
+                sorting: {
+                sortBy: validSortBy,
+                sortOrder: validSortOrder
+                }
+            }
+            });
+
+        } catch (error) {
+            
+            console.error('Error:', error);
+
+            res.status(500).json({
+            error: 'Unable to retrieve applications.',
+            details: error.message
+            });
+        }
+    }
+    
+    static async getOne(req, res) {
+        try {
+            const { id } = req.params;
+
+        
+            const application = await Application.findOne({
+            where: { id }
+            });
+
+            if (!application) {
+            return res.status(404).json({
+                error: 'Not found',
+                details: 'application not found.'
+            });
+            }
+
+            res.status(200).json({
+            message: 'application retrieved successfully.',
+            application
+            });
+
+        } catch (error) {
+
+            console.error('Error:', error);
+
+            res.status(500).json({
+            error: 'Unable to retrieve application.',
+            details: error.message
+            });
+        }
+    }
 
   static async store(req, res) {
     try {
