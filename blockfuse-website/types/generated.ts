122b5f94cde99f75d12efb871cb661ba423058b3
Jethrolopwus <lopwusjethro92@gmail.com>
2024-11-11 14:30:35 +0100

    chre: Api integration for team and cohorts

--- blockfuse-website/types/generated.ts
+++ blockfuse-website/types/generated.ts
@@ -1,18 +1,12 @@
 
-export declare type Team = {
-    id: ["string"];
-    fullname: ["string"];
-    position:["string"];
-    about: ["string"];
-    slug: ["string"];
-    image: ["string"];
-    createdAt: ["Date"];
-    updatedAt: ["Date"];
+export declare type TeamMember = {
+  id: string;
+  name: string;
+  image: string;
+  stack: string;
+  twitter?: string;
+  github?: string;
+  linkedin?: string;
   };
-  export declare type Registeration = {
-    fullname: ["String"]
-    email:["String"]
-    phoneNumber:["String"]
-    password: ["String"]
-  }
+ 
   
