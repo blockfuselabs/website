66030bf690c685f58a93e500d2cc704ab52dc4df
Emmanuel Doji <devopsnormad@gmail.com>
2025-02-28 23:52:03 +0100

    bootcamp registration page open

--- blockfuse-website/src/components/BootcampCards.tsx
+++ blockfuse-website/src/components/BootcampCards.tsx
@@ -8,35 +8,34 @@ import { NavLink } from "react-router-dom";
 const StyledCards = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-6 p-4">
+
+
+
       {/* Card 1 */}
       <div className="dark:bg-black h-auto sm:h-[650px] border border-purple-500 shadow-lg overflow-hidden">
         <div
-          className="h-40 sm:h-60 bg-cover dark:bg-black m-3 sm:m-5 bg-center"
+          className="h-40 sm:h-60 m-3 sm:m-5 bg-cover bg-center"
           style={{ backgroundImage: `url(${Web2})` }}
         >
           <div className="flex items-center justify-center h-full bg-opacity-50">
             <h2 className="dark:text-white text-lg sm:text-2xl font-semibold">
-              Web2 for Web3 Preparatory Bootcamp
+            Web2 Basic Track
             </h2>
           </div>
         </div>
         <div className="p-4 sm:p-6">
-          <p className="dark:text-gray-300 text-justify mb-4 text-lg sm:text-base">
-            Learn the essentials of modern web development in HTML, CSS,
-            Tailwind CSS, JavaScript, and Node.js/React. This program guides you
-            through creating interactive, responsive websites, providing the
-            skills and confidence to start building real-world projects. Perfect
-            for beginners ready to launch their web journey!
+          <p className="dark:text-gray-300 mb-4 text-justify text-lg sm:text-base">
+          This track is designed for beginners with no prior coding experience who want to start their journey in software development. You will learn HTML, CSS, Version Control System and JavaScript from the ground up, building a solid foundation in web development. By the end of this track, you'll be well-prepared to advance into more specialized areas and pave the way for blockchain development in the future.
           </p>
         </div>
         <div className="flex justify-center mb-3 mx-2 sm:mt-16">
           <a
-            href="https://docs.google.com/forms/d/e/1FAIpQLSerXQAQAfV_fpga4mpHBvjGvfnetyHnCdEmkDeFSishs-vTRQ/viewform?usp=pp_url"
+            href="https://docs.google.com/forms/d/e/1FAIpQLSeXU54y3EkoNWoQtzi1Ov5zmJ2SUmmsdZpSgFEGFfmw8DqfjQ/viewform?usp=dialog"
             target="_blank"
             rel="noopener noreferrer"
           >
             <Button>
-              Join the Waitlist
+              Enroll
               <MoveRight />
             </Button>
           </a>
@@ -46,34 +45,67 @@ const StyledCards = () => {
         </div>
       </div>
 
+
       {/* Card 2 */}
       <div className="dark:bg-black h-auto sm:h-[650px] border border-purple-500 shadow-lg overflow-hidden">
+        <div
+          className="h-40 sm:h-60 bg-cover dark:bg-black m-3 sm:m-5 bg-center"
+          style={{ backgroundImage: `url(${Web2})` }}
+        >
+          <div className="flex items-center justify-center h-full bg-opacity-50">
+            <h2 className="dark:text-white text-lg sm:text-2xl font-semibold">
+            Web2 Advanced Track
+            </h2>
+          </div>
+        </div>
+        <div className="p-4 sm:p-6">
+          <p className="dark:text-gray-300 text-justify mb-4 text-lg sm:text-base">
+          This track is designed for those with basic programming knowledge who want to advance in Web2 development. You will dive into advanced JavaScript, React, Node.js & Express, DevOps, and Databases, gaining the skills needed to build and deploy full-stack applications. This program will make you a developer well-prepared to dive into a blockchain programming bootcamp, equipping you with the essential technical foundation for Web3 development
+          </p>
+        </div>
+        <div className="flex justify-center mb-3 mx-2 sm:mt-16">
+          <a
+            href="https://docs.google.com/forms/d/e/1FAIpQLSf_QPUkN_KIuTw4BA-aWTzD8njbvdOJdObwe6mCkzcZikY5PA/viewform?usp=dialog"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Button>
+            Enroll
+              <MoveRight />
+            </Button>
+          </a>
+        </div>
+        <div className="dark:text-sky-400 font-bold italic flex justify-center mb-3 mx-2">
+          NOTE: Registration for Cohort III will start on March 1st.
+        </div>
+      </div>
+
+
+         {/* Card 3 */}
+         <div className="dark:bg-black h-auto sm:h-[650px] border border-purple-500 shadow-lg overflow-hidden">
         <div
           className="h-40 sm:h-60 m-3 sm:m-5 bg-cover bg-center"
           style={{ backgroundImage: `url(${Web3})` }}
         >
           <div className="flex items-center justify-center h-full bg-opacity-50">
             <h2 className="dark:text-white text-lg sm:text-2xl font-semibold">
-              Web3 Bootcamp
+            Web3 Track
             </h2>
           </div>
         </div>
         <div className="p-4 sm:p-6">
           <p className="dark:text-gray-300 mb-4 text-justify text-lg sm:text-base">
-            Unlock the future of the internet with our Web3 Bootcamp! Learn
-            blockchain fundamentals, smart contracts, and dApps to build
-            decentralized applications. Ideal for beginners eager to explore
-            Web3 technology and launch into the world of blockchain development!
+          This program is designed for skilled programmers who want to dive into blockchain development and build the future of Web3. If you have a strong programming background and are ready to take on the world of smart contracts and decentralized applications, this is for you!
           </p>
         </div>
         <div className="flex justify-center mb-3 mx-2 sm:mt-16">
           <a
-            href="https://docs.google.com/forms/d/e/1FAIpQLSerXQAQAfV_fpga4mpHBvjGvfnetyHnCdEmkDeFSishs-vTRQ/viewform?usp=pp_url"
+            href="https://docs.google.com/forms/d/e/1FAIpQLSdOn-3186jflotdHBk7vpC1umcK7ANBNVeR-i43VcYii1nZhw/viewform?usp=dialog"
             target="_blank"
             rel="noopener noreferrer"
           >
             <Button>
-              Join the Waitlist
+            Enroll
               <MoveRight />
             </Button>
           </a>
