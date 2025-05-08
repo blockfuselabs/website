598b4cdf92e2b17d936fc2537306d4715e8d89f4
Kefas Kingsley <kefasiceking@gmail.com>
2025-05-08 14:21:59 +0100

    feat: changed web2 track details

--- blockfuse-website/src/components/BootcampCards.tsx
+++ blockfuse-website/src/components/BootcampCards.tsx
@@ -12,7 +12,7 @@ const StyledCards = () => {
 
 
       {/* Card 1 */}
-      <div className="dark:bg-black h-auto sm:h-[650px] border border-purple-500 shadow-lg overflow-hidden">
+      {/*<div className="dark:bg-black h-auto sm:h-[650px] border border-purple-500 shadow-lg overflow-hidden">
         <div
           className="h-40 sm:h-60 m-3 sm:m-5 bg-cover bg-center"
           style={{ backgroundImage: `url(${Web2})` }}
@@ -26,6 +26,39 @@ const StyledCards = () => {
         <div className="p-4 sm:p-6">
           <p className="dark:text-gray-300 mb-4 text-justify text-lg sm:text-base">
           This track is designed for beginners with no prior coding experience who want to start their journey in software development. You will learn HTML, CSS, Version Control System and JavaScript from the ground up, building a solid foundation in web development. By the end of this track, you'll be well-prepared to advance into more specialized areas and pave the way for blockchain development in the future.
+          </p>
+        </div>
+        <div className="flex justify-center mb-3 mx-2 sm:mt-16">
+          <a
+            href="https://docs.google.com/forms/d/e/1FAIpQLSeXU54y3EkoNWoQtzi1Ov5zmJ2SUmmsdZpSgFEGFfmw8DqfjQ/viewform?usp=dialog"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Button>
+              Enroll
+              <MoveRight />
+            </Button>
+          </a>
+        </div>
+        {/* <div className="dark:text-sky-400 font-bold italic flex justify-center mb-3 mx-2">
+          NOTE: Registration for Cohort III will start on March 1st.
+        </div> */}
+      {/* </div> */}
+
+      <div className="dark:bg-black h-auto sm:h-[650px] border border-purple-500 shadow-lg overflow-hidden">
+        <div
+          className="h-40 sm:h-60 m-3 sm:m-5 bg-cover bg-center"
+          style={{ backgroundImage: `url(${Web2})` }}
+        >
+          <div className="flex items-center justify-center h-full bg-opacity-50">
+            <h2 className="dark:text-white text-lg sm:text-2xl font-semibold">
+            Web2 Software Engineering
+            </h2>
+          </div>
+        </div>
+        <div className="p-4 sm:p-6">
+          <p className="dark:text-gray-300 mb-4 text-justify text-lg sm:text-base">
+          The Web2 Software Engineering track at Blockfuse Labs is a full-spectrum program designed to take you from beginner to industry-ready developer — even if you're starting from scratch. You'll begin by learning the fundamentals of software development: HTML, CSS, JavaScript, and Version Control Systems — building a solid foundation in web development. From there, you'll advance into modern frontend and backend technologies like React, Node.js & Express, along with essential tools like Databases and DevOps. By the end of this program, you'll be able to build and deploy full-stack applications confidently — and you'll be fully prepared to transition into Web3 and blockchain development. This is the path to becoming a well-rounded, future-ready engineer — and it starts here.
           </p>
         </div>
         <div className="flex justify-center mb-3 mx-2 sm:mt-16">
